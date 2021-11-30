1d53be29262073a107afd147811e208e94f4257b
Arhaant <anshuman.bhagowati@gmail.com>
2021-11-30 17:09:56 +0530

    we couldnt hear each other

--- Navigation/TabNavigator.js
+++ Navigation/TabNavigator.js
@@ -1,37 +1,59 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import Feed from '../Screens/Feed'
-import CreateStory from '../Screens/CreateStory';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
 
-const Tab = createBottomTabNavigator();
+import Feed from "../Screens/Feed";
+import CreateStory from "../Screens/CreateStory";
 
+const Tab = createMaterialBottomTabNavigator();
 
-export default class TabNavigator extends Component {
-    render() {
-        return (
-          <Tab.Navigator 
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
-    
-                if (route.name === 'Feed Screen') {
-                  iconName = focused ? 'book' : 'book-outline';
-                } else if (route.name === 'Create Story Screen') {
-                  iconName = focused ? 'create' : 'create-outline';
-                }
-    
-                // You can return any component that you like here!
-                return <Ionicons name={iconName} size={size} color={color} />;
-              },
-              tabBarActiveTintColor: 'blue',
-              tabBarInactiveTintColor: 'black',
-            })}>
-            <Tab.Screen name="Feed Screen" component={Feed} />
-            <Tab.Screen name="Create Story Screen" component={CreateStory} />
-          </Tab.Navigator>
-    )
-}
-}
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      labeled={false}
+      barStyle={styles.bottomTabStyle}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Feed") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Create Story") {
+            iconName = focused ? "add-circle" : "add-circle-outline";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              size={RFValue(25)}
+              color={color}
+              style={styles.icons}
+            />
+          );
+        }
+      })}
+      activeColor={"#ee8249"}
+      inactiveColor={"gray"}
+    >
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Create Story" component={CreateStory} />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: "#2f345d",
+    height: "8%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    overflow: "hidden",
+    position: "absolute"
+  },
+  icons: {
+    width: RFValue(30),
+    height: RFValue(30)
+  }
+});
+
+export default BottomTabNavigator;
--- Screens/Feed.js
+++ Screens/Feed.js
@@ -1,16 +1,108 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Platform, StatusBar, Image } from 'react-native';
+import { RFValue } from "react-native-responsive-fontsize";
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import { FlatList } from 'react-native-gesture-handler';
+import StoryCard from './StoryCard'
+
+
+let customFont = {
+    "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+}
+
+
+let stories = require("./temp_stories.json");
+
 
 export default class Feed extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fontsLoaded: false
+        }
+    }
+
+
+    async loadFontsAsync() {
+        await Font.loadAsync(customFont)
+        this.setState({
+            fontsLoaded: true
+        })
+    }
+
+
+    componentDidMount() {
+        this.loadFontsAsync();
+    }
+
+    renderItem = ({ item: story }) => {
+        return <StoryCard story={story}/>
+    }
+
+
     render() {
-        return (
-            <View  style={{
-                flex: 1,
-                justifyContent: "center",
-                 alignItems: "center"
-                            }}>
-                  <Text>Feed</Text>
-              </View>
-    )
+
+        if (!this.state.fontsLoaded) {
+            return <AppLoading />
+        }
+        else {
+            return (
+                <View style={styles.container}>
+                   <View style={styles.appTitle}>
+                        <View  style={styles.appIcon}>
+                            <Image source={require("../assets/logo.png")} style={styles.iconImage}></Image>
+                        </View>
+                        <View style={styles.appTitleTextContainer}>
+                            <Text style={styles.appTitleText}>Storytelling App</Text>
+                        </View>
+                   </View>
+
+                   <View style={styles.cardContainer}>
+                       <FlatList
+                            keyExtractor = {(item,index)=>index.toString()}
+                            data = {stories}
+                            renderItem ={this.renderItem}
+                       />
+
+                   </View>
+                </View >
+            )
+        }
+    }
 }
-}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#15193c"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+      },
+    appTitle:{
+        flex:0.07,
+        flexDirection:'row'
+    },
+    appIcon:{
+        flex:0.3,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    iconImage:{
+        width:'100%',
+        height:'100%',
+        resizeMode:'contain'    
+    },
+    appTitleTextContainer:{
+        flex:0.7,
+        justifyContent: "center"
+    },
+    appTitleText:{
+        color:'white',
+        fontSize:RFValue(30),
+        fontFamily:'Bubblegum-Sans'
+    }
+
+})
